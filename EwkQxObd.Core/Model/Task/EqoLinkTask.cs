a8a42db18aa3700c06f8febc3d7eeda6e3a5f43a
mchenf <mochen@foss.dk>
2025-04-21 17:35:57 +0800

    Added Task object folder

---
+++ EwkQxObd.Core/Model/Task/EqoLinkTask.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EwkQxObd.Core.Model.Task
+{
+    public class EqoLinkTask
+    {
+        public IqxNetwork Network { get; set; }
+        public EqoAccount Account { get; set; }
+    }
+}
