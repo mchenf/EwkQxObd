7516935ede67b91ece31978e1a5b49c4e4af47a3
mchenf <mochen@foss.dk>
2025-04-21 14:31:14 +0800

    Created class EqoContract

---
+++ EwkQxObd.Core/Model/EqoContract.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EwkQxObd.Core.Model
+{
+    public class EqoContract
+    {
+        public uint ContractNumber { get; set; }
+        public EqoAccount ShipTo { get; set; }
+        public DateOnly ValidFrom { get; set; }
+        public DateOnly ValidTo { get; set; }
+    }
+}
