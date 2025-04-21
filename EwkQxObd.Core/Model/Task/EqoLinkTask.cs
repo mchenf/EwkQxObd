24611ff49908f6813bcec5a3b8062a9516f988ef
mchenf <mochen@foss.dk>
2025-04-21 17:37:26 +0800

    Created ctor for this class

--- EwkQxObd.Core/Model/Task/EqoLinkTask.cs
+++ EwkQxObd.Core/Model/Task/EqoLinkTask.cs
@@ -9,6 +9,14 @@ namespace EwkQxObd.Core.Model.Task
     public class EqoLinkTask
     {
         public IqxNetwork Network { get; set; }
-        public EqoAccount Account { get; set; }
+        public EqoAccount? Before { get => Network.Account; }
+        public EqoAccount After { get; set; }
+        public DateOnly StatusUpdated { get; set; }
+
+        public EqoLinkTask(IqxNetwork network, EqoAccount LinkTo)
+        {
+            Network = network;
+            After = LinkTo;
+        }
     }
 }
