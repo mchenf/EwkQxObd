0d6a218d2fc4cde00432fc8c07c7d2c971382ea0
mchenf <mochen@foss.dk>
2025-04-21 17:29:34 +0800

    Added class IqxSystem

---
+++ EwkQxObd.Core/Model/IqxNetwork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EwkQxObd.Core.Model
+{
+    public class IqxNetwork
+    {
+        public IqxSystem System { get; set; }
+        public Guid NetworkGuid { get; set; }
+        public EqoAccount? Account { get; set; }
+
+        public IqxNetwork(IqxSystem system)
+        {
+            System = system;
+        }
+    }
+}
