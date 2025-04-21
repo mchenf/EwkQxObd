2e5371677745cd52120689cf4789f60c305ef820
mchenf <mochen@foss.dk>
2025-04-21 14:36:47 +0800

    Created class EqoAccount

---
+++ EwkQxObd.Core/Model/EqoAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EwkQxObd.Core.Model
+{
+    public class EqoAccount
+    {
+        public uint AccountID { get; set; }
+        public string PartnerName { get; set; } = string.Empty;
+    }
+}
