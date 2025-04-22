0ea3f877605b18ac3b3fe89250f22c03999f49b2
mchenf <mochen@foss.dk>
2025-04-22 13:42:22 +0800

    Created DataStore Class

---
+++ EwkQxObd.Data/EqoDataStore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EwkQxObd.Data
+{
+    public class EqoDataStore : IDisposable
+    {
+
+
+        public void CreateDB()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
