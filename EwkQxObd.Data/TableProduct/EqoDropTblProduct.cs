f1db9699cba0ae060e559aa11939fda7fd9a9371
mchenf <mochen@foss.dk>
2025-04-29 13:33:40 +0800

    Created class for dropping tables

---
+++ EwkQxObd.Data/TableProduct/EqoDropTblProduct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EwkQxObd.Data.TableProduct
+{
+    public class EqoDropTblProduct : EqoDataStoreBase
+    {
+        private const string Cmdtx_Drp_Tbl_Product
+= @"
+DROP TABLE IF EXISTS Eqo_Product;
+";
+        public void DropTable()
+        {
+            OpenConnDoStuff(() =>
+            {
+                var command = Connection.CreateCommand();
+
+                command.CommandText = Cmdtx_Drp_Tbl_Product;
+                command.ExecuteNonQuery();
+            });
+        }
+    }
+}
