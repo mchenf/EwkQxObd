a0f0d6b72ce15b5518172100df854c64a46e1570
mchenf <mochen@foss.dk>
2025-05-15 12:59:11 +0800

    Moved installation folder

---
+++ EwkQxObd.Pwsh/Installation/InstallationBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Management.Automation;
+
+namespace EwkQxObd.Pwsh.Installation
+{
+    public abstract class InstallationBase
+    {
+        private const string rootDir = "EwkQxObd.Foss.Mochen";
+        private const string database = "appdata.db";
+
+
+        protected InstallationBase()
+        {
+            
+        }
+    }
+}
