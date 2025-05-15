4b6194780a5c00792c3f0ca06c89880c590bff54
mchenf <mochen@foss.dk>
2025-05-15 10:48:59 +0800

    Created cmdlet InstallationInitialize

---
+++ EwkQxObd.Pwsh/InstallationInitialize.cs
@@ -0,0 +1,16 @@
+﻿using System.Management.Automation;
+
+namespace EwkQxObd.Pwsh
+{
+    [Cmdlet(VerbsData.Initialize, "Installation")]
+    public class InstallationInitialize : Cmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            WriteDebug("InstallationInitialize starts");
+
+
+
+        }
+    }
+}
