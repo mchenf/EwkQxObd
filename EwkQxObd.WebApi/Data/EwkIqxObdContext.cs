fedd7b449020f55e619b809393476bac7ecb66e8
mchenf <mochen@foss.dk>
2025-07-25 16:46:20 +0800

    Adjusted object model, added context object

---
+++ EwkQxObd.WebApi/Data/EwkIqxObdContext.cs
@@ -0,0 +1,25 @@
+﻿using EwkQxObd.Core.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EwkQxObd.WebApi.Data
+{
+    public class EwkIqxObdContext : DbContext
+    {
+        public EwkIqxObdContext(DbContextOptions<EwkIqxObdContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<IqxSystem> IqxSystems { get; set; }
+        public DbSet<IqxNetwork> IqxNetworks { get; set; }
+        public DbSet<IqxOrganization> IqxOrganisation { get; set; }
+
+
+        public DbSet<EqoContractObject> EqoContractObject { get; set; }
+        public DbSet<EqoContract> EqoContract { get; set; }
+        public DbSet<EqoContactInfo> EqoContactInfo { get; set; }
+        public DbSet<EqoAccount> EqoAccount { get; set; }
+
+
+    }
+}
