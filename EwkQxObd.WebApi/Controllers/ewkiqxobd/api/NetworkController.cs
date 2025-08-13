e826b12fa3fe0cd6f371e021bed7dd772a75fa90
mchenf <mochen@foss.dk>
2025-08-13 15:30:37 +0800

    Added multi file upload endpoint

--- EwkQxObd.WebApi/Controllers/ewkiqxobd/api/NetworkController.cs
+++ EwkQxObd.WebApi/Controllers/ewkiqxobd/api/NetworkController.cs
@@ -43,79 +43,81 @@ namespace EwkQxObd.WebApi.Controllers.ewkiqxobd.api
         [HttpPost("csv/upload")]
         [Consumes("multipart/form-data")]
         [Produces("application/json")]
-        public async Task<IActionResult> UploadCsv(IFormFile csvFile)
+        public async Task<IActionResult> UploadCsv(IFormFileCollection csvFiles)
         {
-            if (csvFile == default || csvFile.Length == 0)
+            Dictionary<string, int> report = [];
+            foreach (var csvFile in csvFiles)
             {
-                return BadRequest("No file uploaded or file is empty.");
-            }
-            if (csvFile.ContentType != "text/csv")
-            {
-                return BadRequest("Uploaded file is not recognized as a csv file.");
-            }
 
-            using var memStream = new MemoryStream();
-
-            await csvFile.CopyToAsync(memStream);
-
-            memStream.Seek(0, SeekOrigin.Begin);
-
-            using var reader = new StreamReader(memStream, Encoding.UTF8);
-
-            
-            List<IqxNetworkInstrument> results = new();
-
-            while (!reader.EndOfStream)
-            {
-                string? line = await reader.ReadLineAsync();
-                if (string.IsNullOrEmpty(line))
+                if (csvFile == default || csvFile.Length == 0)
                 {
-                    continue;
+                    return BadRequest("No file uploaded or file is empty.");
                 }
-                var deserilized = line.Deserialize();
-                bool checkToSync = true;
-                if (deserilized != default)
+                if (csvFile.ContentType != "text/csv")
                 {
-                    //Finally gets deserialized
-                    //Check 2 things.
-                    //1) Does the database contain the timestamp
-                    //2) Does it contains the system
-                    //If this can be found, refuse to do anything and return BadRequest
-                    if (checkToSync)
+                    return BadRequest("Uploaded file is not recognized as a csv file.");
+                }
+
+                using var memStream = new MemoryStream();
+
+                await csvFile.CopyToAsync(memStream);
+
+                memStream.Seek(0, SeekOrigin.Begin);
+
+                using var reader = new StreamReader(memStream, Encoding.UTF8);
+
+                List<IqxNetworkInstrument> results = [];
+
+
+                bool checkToSync = true;
+                while (!reader.EndOfStream)
+                {
+                    string? line = await reader.ReadLineAsync();
+                    if (string.IsNullOrEmpty(line))
                     {
-
-                        var tryToFind = await _context.IqxInstrument.FirstOrDefaultAsync(
-                            a =>
-                            a.QueryTimeStamp == deserilized.QueryTimeStamp &&
-                            a.System == deserilized.System);
-
-                        if (tryToFind != null)
+                        continue;
+                    }
+                    var deserilized = line.Deserialize();
+                    if (deserilized != default)
+                    {
+                        //Finally gets deserialized
+                        //Check 2 things.
+                        //1) Does the database contain the timestamp
+                        //2) Does it contains the system
+                        //If this can be found, refuse to do anything and return BadRequest
+                        if (checkToSync)
                         {
-                            return BadRequest(
-                                "File is already sycned with duplicated query time & system." +
-                                $"Query Time: {tryToFind.QueryTimeStamp};" +
-                                $"System: {tryToFind.System};" +
-                                $"Try to upload a different query csv."
-                            );
+                            //This block runs only on the first time.
+                            report.Add(deserilized.System, 0);
+                            var tryToFind = await _context.IqxInstrument.FirstOrDefaultAsync(
+                                a =>
+                                a.QueryTimeStamp == deserilized.QueryTimeStamp &&
+                                a.System == deserilized.System);
+
+
+                            if (tryToFind != null)
+                            {
+                                report[deserilized.System] = -1;
+                                break;
+                            }
+                            else
+                            {
+                                checkToSync = false;
+                            }
                         }
-                        else
-                        {
-                            checkToSync = false;
-                        }
+
+                        report[deserilized.System]++;
+                        results.Add(deserilized);
                     }
 
 
-                    results.Add(deserilized);
                 }
 
-
+                await _context.IqxInstrument.AddRangeAsync(results);
+                await _context.SaveChangesAsync();
             }
 
-            await _context.IqxInstrument.AddRangeAsync(results);
-
-            await _context.SaveChangesAsync();
-
-            return Ok(results);
+            return Ok(report);
         }
 
     }
