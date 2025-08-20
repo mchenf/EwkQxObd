716f1e936ced5af149f758934b5f51087f9ffd7d
mchenf <mochen@foss.dk>
2025-08-20 14:28:52 +0800

    Added views for contract object

---
+++ EwkiQxobd.Web/Controllers/ContractObjectController.cs
@@ -0,0 +1,93 @@
+﻿using EwkQxObd.WebApi.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EwkiQxobd.Web.Controllers
+{
+    public class ContractObjectController : Controller
+    {
+        private EwkIqxObdContext _context;
+        private ILogger<ContractObjectController> _logger;
+
+        public ContractObjectController(EwkIqxObdContext ctx, ILogger<ContractObjectController> logger)
+        {
+            _context = ctx;
+            _logger = logger;
+        }
+        // GET: ContractObjectController
+        public async Task<ActionResult> Index()
+        {
+            return View(await _context.VwSysnetinstorg.ToListAsync());
+        }
+
+        // GET: ContractObjectController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ContractObjectController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ContractObjectController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ContractObjectController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ContractObjectController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ContractObjectController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ContractObjectController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ EwkiQxobd.Web/Data/EwkIqxObdContext.cs
@@ -0,0 +1,44 @@
+﻿using EwkQxObd.Core.Model;
+using EwkQxObd.Core.Model.Views;
+using Microsoft.EntityFrameworkCore;
+
+namespace EwkQxObd.WebApi.Data
+{
+    public class EwkIqxObdContext : DbContext
+    {
+        public EwkIqxObdContext(DbContextOptions<EwkIqxObdContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<IqxSystem> IqxSystem { get; set; }
+        public DbSet<IqxNetworkInstrument> IqxInstrument { get; set; }
+        public DbSet<IqxOrganization> IqxOrganisation { get; set; }
+
+
+        public DbSet<EqoContractObject> EqoContractObject { get; set; }
+        public DbSet<EqoContract> EqoContract { get; set; }
+        public DbSet<EqoContactInfo> EqoContactInfo { get; set; }
+        public DbSet<EqoAccount> EqoAccount { get; set; }
+
+
+        public DbSet<EqoTicketSource> EqoTicketSource { get; set; }
+
+        public DbSet<vwSysnetinst> VwSysnetinst { get; set; }
+        public DbSet<vwSysnetinstorg> VwSysnetinstorg { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<vwSysnetinst>(ent => {
+                ent.ToView("vw_LatestSysnetinst");
+                ent.HasNoKey();
+            });
+
+            modelBuilder.Entity<vwSysnetinstorg>(ent => {
+                ent.ToView("vw_LatestSysnetinstorg");
+                ent.HasNoKey();
+            });
+
+        }
+    }
+}
