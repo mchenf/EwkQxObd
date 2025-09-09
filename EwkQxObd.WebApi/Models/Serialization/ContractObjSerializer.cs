e41f07394e9b1e35e50b5cf2b404b319d9f741e3
mchenf <mochen@foss.dk>
2025-09-09 10:44:30 +0800

    Created a serializer to convert object to flat text

---
+++ EwkQxObd.WebApi/Models/Serialization/ContractObjSerializer.cs
@@ -0,0 +1,64 @@
+﻿using EwkQxObd.Core.Model;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Text;
+
+namespace EwkQxObd.WebApi.Models.Serialization
+{
+    public static class ContractObjSerializer
+    {
+        private const string format1 = @"
+==== Contract ====
+Contract Number: {0}
+Description: {1}
+Customer Contact: {2}
+Employee Responsible: {3}
+Valid From: {4}
+Valid To: {5}
+
+==== Ship-To ====
+Account Number: {6}
+Account Name: {7}
+Region: {8}
+Country: {9}
+
+==== Instrument ====
+Serial Number: {10}
+Instrument Type: {11}
+";
+
+
+        public static string ToFlatText(this EqoContractObject obj)
+        {
+            string emailCustomer = obj.Contract!.CustomerContact is null ?
+                "<Not Specified>" :
+                string.Format("{0}<{1}>", obj.Contract.CustomerContact.FullName,
+                    obj.Contract.CustomerContact.EmailAddress ?? "email@not.exist"
+                );
+
+            string emailEmployee = obj.Contract!.EmployeeResponsible is null ?
+                "<Not Specified>" :
+                string.Format("{0}<{1}>", obj.Contract.EmployeeResponsible.FullName,
+                    obj.Contract.EmployeeResponsible.EmailAddress ?? "email@not.exist"
+                );
+
+            string result = string.Format(format1,
+                obj.Contract.ContractNumber,
+                obj.Contract.Description,
+                emailCustomer,
+                emailEmployee,
+                obj.Contract.ValidFrom,
+                obj.Contract.ValidTo,
+                obj.ShipTo!.PartnerId,
+                obj.ShipTo!.PartnerName,
+                obj.ShipTo!.Region,
+                obj.ShipTo!.Country,
+                obj.SerialNumber,
+                obj.InstrumentType
+            );
+
+
+
+            return result;
+        }
+    }
+}
