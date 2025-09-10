42a7bca7df33815c173ddae56a1fd1f5cfd063df
mchenf <mochen@foss.dk>
2025-09-10 12:07:05 +0800

    Added copy to clipboard function

---
+++ EwkQxObd.WebApi/wwwroot/js/contractObj/copyToClipb.js
@@ -0,0 +1,24 @@
+﻿// Allowing a button's click to copy to clipboard
+//author: mochen@foss.dk
+//version: 1.0.0.0
+//released: 9/9/2025
+
+
+$('#btnCopy2clipB').click(() => {
+    copToClipboard();
+});
+
+function copToClipboard() {
+    var text2Cpy = $('#txaCondensedConObj').val()
+
+    navigator.clipboard.writeText(text2Cpy)
+    .then(() => {
+        console.log('Clipboard text copied');
+    }).then(() => {
+        alert('Content copied...');
+    }).catch(err => {
+        console.error('Failed to copy: ', err);
+    });
+
+}
+
