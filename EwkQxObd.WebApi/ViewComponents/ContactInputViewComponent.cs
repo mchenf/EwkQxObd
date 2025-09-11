b7779e3c6dc63a09cecc5d432d2d8c070692bae6
mchenf <mochen@foss.dk>
2025-09-11 18:00:17 +0800

    Splitted more components. not sure if it will work. Contract Object hasn't implemented

---
+++ EwkQxObd.WebApi/ViewComponents/ContactInputViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using EwkQxObd.Core.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EwkQxObd.WebApi.ViewComponents
+{
+    public class ContactInputViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(EqoContactInfo contact, int tagId)
+        {
+            ViewBag.TagId = tagId;
+
+            return View(contact);
+        }
+    }
+}
