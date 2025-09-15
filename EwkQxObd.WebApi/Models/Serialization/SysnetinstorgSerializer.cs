32d3a27e8c6bdc603a4e0d0341bcd160416f512f
mchenf <mochen@foss.dk>
2025-09-16 01:47:29 +0800

    added search page for sngio search

---
+++ EwkQxObd.WebApi/Models/Serialization/SysnetinstorgSerializer.cs
@@ -0,0 +1,46 @@
+﻿using EwkQxObd.Core.Model.Views;
+
+namespace EwkQxObd.WebApi.Models.Serialization
+{
+    public static class SysnetinstorgSerializer
+    {
+        private const string format1 = @"
+==== Instrument Info ====
+System: {0}
+Network Id: {1}
+Network Name: {2}
+Inst. Group: {3}
+Serial Number: {4}
+
+==== Linked on IQX ====
+Linked To: {5}
+Account Number: {6}
+Account Name: {7}
+Street: {8}
+City: {9}
+";
+
+
+        public static string ToFlatText(this VwSysnetinstorg obj)
+        {
+            
+            string result = string.Format(format1,
+                obj.System,
+                obj.NetworkId,
+                obj.NetworkName,
+                obj.InstrumentGroup,
+                obj.SerialNumber,
+
+                obj.LinkedAccountGuid,
+                obj.AccountNumber,
+                obj.Name,
+                obj.Street,
+                obj.City
+            );
+
+
+
+            return result;
+        }
+    }
+}
