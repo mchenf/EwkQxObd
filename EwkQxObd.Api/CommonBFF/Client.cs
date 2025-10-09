632872b64b1abf3ea3e16e5b9fe52d1c7c61178c
mchenf <mochen@foss.dk>
2025-10-09 15:53:31 +0800

    Created Common BFF client

---
+++ EwkQxObd.Api/CommonBFF/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EwkQxObd.Api.CommonBFF
+{
+    public class Client : ClientBase
+    {
+        public string UrlCommonBFF { get; set; } = string.Empty;
+
+
+
+
+
+
+    }
+}
