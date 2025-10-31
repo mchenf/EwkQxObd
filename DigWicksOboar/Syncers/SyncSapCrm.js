df7debb709c54edabe33369d9d3bce0651312ec5
mchenf <mochen@foss.dk>
2025-10-31 18:14:18 +0800

    Modified the sync logic so that on click of a button it can spit out objects carrying info

--- DigWicksOboar/Syncers/SyncSapCrm.js
+++ DigWicksOboar/Syncers/SyncSapCrm.js
@@ -12,16 +12,16 @@
 
 (function () {
     'use strict';
-    console.log("GetSAPCRMContract v1.0.1.4");
+    console.log("GetSAPCRMContract v1.0.1.6");
     let d1selector = 'div#C1_W1_V2';
     let div1 = document.querySelectorAll(d1selector)[0];
 
     if (div1 === undefined) {
-        console.log("Unable to find " + d1selector);
-        console.log("Exiting...");
+        console.log("[WARNING]:: Unable to find " + d1selector);
+        console.log("[WARNING]:: Exiting...");
         return;
     }
-    console.log("[Success]:: Target iframe found, Injecting");
+    console.log("[Success]:: Target iframe found, Injecting button element.");
 
 
     let button =
@@ -33,14 +33,65 @@
         'click',
         function () {
             let contentTds = document.querySelectorAll('div.ch-grid-of');
-            for (let x = 0; x < contentTds.length; x++) {
-                console.log("" + x + ", " + contentTds[x].innerText);
+
+            //Verify current page contains contract info, if yes, split out a contract object
+
+            let syncContractReady = {};
+
+            syncContractReady.GD = contentTds[0].innerText === 'General Data';
+            syncContractReady.ID = contentTds[2].innerText === 'ID:';
+            syncContractReady.DE = contentTds[5].innerText === 'Description:';
+
+            let syncEquipmReady = {};
+            syncEquipmReady.GD = contentTds[1].innerText === 'General Data';
+            syncEquipmReady.ID = contentTds[3].innerText === 'Equipment ID:';
+            syncEquipmReady.DE = contentTds[6].innerText === 'Equipment:';
 
 
+            console.log(syncContractReady);
+            console.log(syncEquipmReady);
 
+            if (
+                syncContractReady.GD &&
+                syncContractReady.ID &&
+                syncContractReady.DE
+            ) {
 
+                let oContract = {};
+                oContract.ContractNumber = contentTds[3].innerText;
+                oContract.Description = contentTds[6].innerText;
+                oContract.SoldToParty = contentTds[9].innerText;
+                oContract.CustomerContact = contentTds[12].innerText;
+                oContract.EmployeeResponsible = contentTds[15].innerText;
+
+                oContract.ContractStart = contentTds[54].innerText;
+                oContract.ContractEnd = contentTds[58].innerText;
+
+                console.log(oContract);
             }
+            else if (
+                syncEquipmReady.GD &&
+                syncEquipmReady.ID &&
+                syncEquipmReady.DE
+            ) {
+                let oContractObj = {};
+                oContractObj.SerialNumber = contentTds[40].innerText;
+                oContractObj.InstrumentType = contentTds[7].innerText;
+                console.log(oContractObj);
+            }
+            else {
+                for (let x = 0; x < contentTds.length; x++) {
+                    console.log("" + x + ", " + contentTds[x].innerText);
+                }
+            }
+
+
             let shipToTds = document.querySelectorAll('td.th-clr-cel.th-clr-td.th-clr-pad.th-clr-cel-dis');
+
+            //TODO: Need sync logic as below:
+            //If the cell is "Ship-To Party", pos = n
+            //n-2 = Account ID
+            //n-1 = Partner name
             for (let x = 0; x < shipToTds.length; x++) {
                 console.log("" + x + ", " + shipToTds[x].innerText);
 
