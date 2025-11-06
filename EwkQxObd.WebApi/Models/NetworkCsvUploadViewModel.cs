b9859ce2d31cfc5e36da0c6e4b845ad822f0c00a
mchenf <mochen@foss.dk>
2025-11-06 17:59:30 +0800

    Changed the default behavior to false for FromWebCall prop

--- EwkQxObd.WebApi/Models/NetworkCsvUploadViewModel.cs
+++ EwkQxObd.WebApi/Models/NetworkCsvUploadViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class NetworkCsvUploadViewModel
     {
-        public bool FromWebCall { get; set; } = true;
+        public bool FromWebCall { get; set; } = false;
         public IFormFileCollection? Files { get; set; }
     }
 }
