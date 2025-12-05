0f89bbb83e85b54dc7d119f040a31c4ad96f5e19
mchenf <mochen@foss.dk>
2025-12-05 14:38:45 +0800

    Created Task work flow page

--- EwkQxObd.WebApi/Controllers/TaskWorkFlowController.cs
+++ EwkQxObd.WebApi/Controllers/TaskWorkFlowController.cs
@@ -1,21 +1,44 @@
 ﻿using EwkQxObd.Core.Model;
+using EwkQxObd.WebApi.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace EwkQxObd.WebApi.Controllers
 {
     [Route("[controller]/[action]")]
-    public class TaskWorkFlowController : Controller
+    public class TaskWorkFlowController(EwkIqxObdContext context, ILogger<TaskWorkFlowController> logger) : Controller
     {
+        private const string TwfCreateMsg = "Added TWF: {result}";
+        private readonly EwkIqxObdContext _context = context;
+        private readonly ILogger<TaskWorkFlowController> _logger = logger;
+
         [HttpGet]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var Results = await _context.TaskWorkFlow.ToListAsync();
+            return View(Results);
         }
 
         [HttpGet]
         public IActionResult Create()
         {
             return View(new EqoTaskWorkflow());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(EqoTaskWorkflow WorkFlow)
+        {
+            _logger.BeginScope("Create()");
+            var twf = await _context.TaskWorkFlow.AddAsync(WorkFlow);
+            _logger.LogDebug(TwfCreateMsg, twf);
+
+            var result = await _context.SaveChangesAsync();
+
+            _logger.LogDebug(TwfCreateMsg, result);
+
+            return RedirectToAction(nameof(Index));
+
         }
     }
 }
