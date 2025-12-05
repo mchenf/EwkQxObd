74f93b6d9eed376a8d402e625f10b1c4e8620449
mchenf <mochen@foss.dk>
2025-12-05 13:31:33 +0800

    Created a page to create twf objects

---
+++ EwkQxObd.WebApi/Controllers/TaskWorkFlowController.cs
@@ -0,0 +1,21 @@
+﻿using EwkQxObd.Core.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EwkQxObd.WebApi.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class TaskWorkFlowController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new EqoTaskWorkflow());
+        }
+    }
+}
