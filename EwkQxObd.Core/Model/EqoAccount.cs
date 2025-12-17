ebec2ab601e4236f01b3922c793a12ac9a96df7a
mchenf <mochen@foss.dk>
2025-12-17 13:01:33 +0800

    Formalized some of the EqoAccount props

--- EwkQxObd.Core/Model/EqoAccount.cs
+++ EwkQxObd.Core/Model/EqoAccount.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -14,12 +15,23 @@ namespace EwkQxObd.Core.Model
     [Table("Account", Schema = "eqo")]
     public class EqoAccount
     {
+        [Key]
         public int Id { get; set; }
+
+        [Column(nameof(PartnerId), TypeName = "int")]
         public int PartnerId { get; set; } = int.MinValue;
+
+        [Column(nameof(PartnerName), TypeName = "nvarchar(64)")]
         public string PartnerName { get; set; } = string.Empty;
+
+        [Column(nameof(GeisID), TypeName = "uniquidentifier")]
         public Guid GeisID { get; set; }
 
+
+        [Column(nameof(Country), TypeName = "nvarchar(16)")]
         public string Country { get; set; } = string.Empty;
+
+        [Column(nameof(Region), TypeName = "nvarchar(16)")]
         public string Region { get; set; } = string.Empty;
 
         //public List<EqoContactInfo>? OrgnisationAdmins { get; set; }
