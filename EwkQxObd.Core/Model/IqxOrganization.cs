986b163cdd2b21ecdb3ee7ee04eed86bd90438a0
mchenf <mochen@foss.dk>
2025-12-17 13:29:22 +0800

    Formalized some of the IQX org props

--- EwkQxObd.Core/Model/IqxOrganization.cs
+++ EwkQxObd.Core/Model/IqxOrganization.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -13,18 +14,30 @@ namespace EwkQxObd.Core.Model
     [Index(nameof(GeisGuid), IsUnique = true)]
     public class IqxOrganization
     {
+        [Key]
         [JsonIgnore]
         public int id { get; set; }
+
+
         [JsonPropertyName("id")]
+        [Column(nameof(GeisGuid), TypeName = "uniqueidentifier")]
         public Guid GeisGuid { get; set; }
+
         [JsonPropertyName("name")]
+        [Column(nameof(Name), TypeName = "nvarchar(64)")]
         public string Name { get; set; } = string.Empty;
+
+
         [JsonPropertyName("accountNumber")]
-        public string AccountNumber { get; set; } = string.Empty;
+        [Column(nameof(AccountNumber), TypeName = "int")]
+        public int AccountNumber { get; set; } = int.MinValue;
 
         [JsonPropertyName("city")]
+        [Column(nameof(City), TypeName = "nvarchar(64)")]
         public string City { get; set; } = string.Empty;
+
         [JsonPropertyName("street")]
+        [Column(nameof(Street), TypeName = "nvarchar(64)")]
         public string? Street { get; set; } = string.Empty;
 
     }
