635fdd0441d8f86ea484e53b3d538e7b08fe0c00
mchenf <mochen@foss.dk>
2026-02-05 15:46:17 +0800

    Fixed the condition judgement for different network detail page status

--- EwkQxObd.Core/Model/Views/VinlkStatus.cs
+++ EwkQxObd.Core/Model/Views/VinlkStatus.cs
@@ -15,8 +15,9 @@ namespace EwkQxObd.Core.Model.Views
         MissContract =      0b_0000_0000_0000_0001,
         MissSystem =        0b_0000_0000_0000_0010,
         MissNetwork =       0b_0000_0000_0000_0100,
-        GeisNotLinked =     0b_0000_0000_0000_1000,
-        MissGeis =          0b_0000_0000_0001_0000,
+        LinkedToZero =      0b_0000_0000_0000_1000,
+
+        LinkedToNull =      0b_0000_0000_0001_0000,
         MissAccount =       0b_0000_0000_0010_0000
     }
 }
