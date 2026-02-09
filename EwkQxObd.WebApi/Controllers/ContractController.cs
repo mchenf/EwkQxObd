6daac27467b05b477b42a60e7fcc761be5b06fff
mchenf <mochen@foss.dk>
2026-02-09 14:03:12 +0800

    Added detail page for contract items

--- EwkQxObd.WebApi/Controllers/ContractController.cs
+++ EwkQxObd.WebApi/Controllers/ContractController.cs
@@ -1,11 +1,18 @@
 ﻿using EwkQxObd.Core.Model;
+using EwkQxObd.WebApi.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EwkQxObd.WebApi.Controllers
 {
     [Route("[controller]/[action]")]
-    public class ContractController : Controller
+    public class ContractController(
+        EwkIqxObdContext ctx
+        ) : Controller
     {
+
+        private readonly EwkIqxObdContext _context = ctx;
+
         [HttpGet]
         public IActionResult Index()
         {
@@ -28,6 +35,27 @@ namespace EwkQxObd.WebApi.Controllers
             }
 
             throw new NotImplementedException();
+
+        }
+
+        [HttpGet]
+        [Route("{ContractNo}")]
+        public async Task<IActionResult> Detail([FromRoute] int ContractNo)
+        {
+            var contracts = await _context.InstrumentLinkStatus
+                .Where(I => I.ContractNumber == ContractNo)
+                .Include(I => I.ShippedTo)
+                .Include(I => I.ConnectedTo)
+                .ToListAsync();
+
+            if (contracts is null)
+            {
+                return NoContent();
+            }
+            else
+            {
+                return View(contracts);
+            }
 
         }
 
