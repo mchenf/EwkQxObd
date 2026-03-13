e7dd63d38e93fd4023171cbd1b31cafed4028d1c
mchenf <mochen@foss.dk>
2026-03-13 14:58:30 +0800

    Added IQX User API Controller

---
+++ EwkQxObd.Core/Model/IqxUser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace EwkQxObd.Core.Model
+{
+    [Table("User", Schema ="iqx")]
+    public class IqxUser
+    {
+        [Key]
+        [Column(nameof(UserId), TypeName = "int")]
+        public int UserId { get; set; }
+
+
+        [JsonPropertyName("id")]
+        [Column(nameof(UserGuid), TypeName = "uniqueidentifier")]
+        public Guid UserGuid { get; set; }
+
+        [JsonPropertyName("email")]
+        [Column(nameof(Email), TypeName = "nvarchar(50)")]
+        public string Email { get; set; } = string.Empty;
+
+        [JsonPropertyName("firstName")]
+        [Column(nameof(FirstName), TypeName = "nvarchar(50)")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [JsonPropertyName("lastName")]
+        [Column(nameof(LastName), TypeName = "nvarchar(50)")]
+        public string LastName { get; set; } = string.Empty;
+
+        [JsonPropertyName("locked")]
+        [Column(nameof(Locked), TypeName = "bit")]
+        public bool Locked { get; set; }
+
+        [JsonPropertyName("organization")]
+        [Column(nameof(OrgGuid), TypeName = "uniqueidentifier")]
+        public Guid OrgGuid { get; set; }
+
+        [JsonPropertyName("queriedAt")]
+        [Column(nameof(QueriedAt), TypeName = "datetime2(7)")]
+        public DateTime QueriedAt { get; set; }
+
+    }
+}
---
+++ EwkQxObd.WebApi/Controllers/ewkiqxobd/api/UserController.cs
@@ -0,0 +1,26 @@
+﻿using EwkQxObd.WebApi.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EwkQxObd.WebApi.Controllers.ewkiqxobd.api
+{
+    [ApiController]
+    [Route("ewkiqxobd/api/[controller]/[action]")]
+    public class UserController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly EwkIqxObdContext _context;
+
+        public UserController(ILogger<UserController> logger, EwkIqxObdContext dataContext)
+        {
+            _logger = logger;
+            _context = dataContext;
+        }
+
+        [HttpPost]
+        [Consumes("application/json")]
+        public IActionResult New()
+        {
+            return View();
+        }
+    }
+}
