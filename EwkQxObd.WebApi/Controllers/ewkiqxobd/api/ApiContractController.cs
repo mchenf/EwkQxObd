441a7427417c588a06248ec73af7bdb3bc45ef10
mchenf <mochen@foss.dk>
2026-05-27 18:22:55 +0800

    Added full CRUD suite for this endpoint

--- EwkQxObd.WebApi/Controllers/ewkiqxobd/api/ApiContractController.cs
+++ EwkQxObd.WebApi/Controllers/ewkiqxobd/api/ApiContractController.cs
@@ -43,8 +43,64 @@ namespace EwkQxObd.WebApi.Controllers.ewkiqxobd.api
             return Result;
         }
 
+        [HttpPut("{ContractId}")]
+        public async Task<IActionResult> Update([FromRoute] int ContractId, [FromBody] EqoContract contract)
+        {
+            if (ContractId != contract.Id)
+            {
+                return BadRequest(new
+                {
+                    Message = "ContractNumber mismatch between route and body."
+                });
+            }
+
+
+            var existing = await _context.EqoContract.
+                FirstOrDefaultAsync(c => c.Id == ContractId);
+            if (existing is null)
+            {
+                return NotFound(new
+                {
+                    Message = $"Contract {contract.Id} cannot be found."
+                });
+            }
+
+            EqoContract old = new()
+            {
+                Id = existing.Id,
+                ContractNumber = existing.ContractNumber,
+                Description = existing.Description,
+                ValidFrom = existing.ValidFrom,
+                ValidTo = existing.ValidTo,
+                CustomerContactId = existing.CustomerContactId,
+                EmployeeResponsibleId = existing.EmployeeResponsibleId,
+                RecordedAt = existing.RecordedAt,
+            };
+
+            existing.ContractNumber = contract.ContractNumber;
+            existing.Description = contract.Description;
+
+            existing.ValidFrom = contract.ValidFrom;
+            existing.ValidTo = contract.ValidTo;
+
+            existing.CustomerContactId = contract.CustomerContactId;
+            existing.EmployeeResponsibleId = contract.EmployeeResponsibleId;
+
+            existing.RecordedAt = contract.RecordedAt;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(new
+            {
+                Message = "Update successful.",
+                Old = old,
+                New = existing
+            });
+
+        }
+
         [HttpGet]
-        public IEnumerable<EqoContract> Details()
+        public IEnumerable<EqoContract> List()
         {
             return _context.EqoContract;
 
@@ -58,13 +114,46 @@ namespace EwkQxObd.WebApi.Controllers.ewkiqxobd.api
 
             await _context.EqoContract.AddAsync(contract);
 
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                var ie = e.InnerException ?? e; 
+                return BadRequest(new
+                {
+                    Message = "Unable to complete database add operation.",
+                    Exception = ie.Message ?? "Not message specified"
+                });
+            }
+
+            return Ok(new { Message = "New contract created successfully", Values = contract });
+        }
+
+
+        [HttpDelete("{ContractId}")]
+        public async Task<IActionResult> Delete([FromRoute] int ContractId)
+        {
+            var found = await _context.EqoContract.FirstOrDefaultAsync(c => c.Id == ContractId);
+
+            if (found is null)
+            {
+                return NotFound(new
+                {
+                    Message = $"No contract with ID: {ContractId} can be found, nothing to delete."
+                });
+            }
+
+            _context.EqoContract.Remove(found);
+
             await _context.SaveChangesAsync();
 
-            return Ok(new { Consumes = "application/json", Values = contract });
-
-
-
-
+            return Ok(new
+            {
+                Message = $"Contract {ContractId} is deleted.",
+                Deleted = found
+            });
         }
 
     }
