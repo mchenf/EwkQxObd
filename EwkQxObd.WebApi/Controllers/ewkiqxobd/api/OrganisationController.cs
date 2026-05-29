1678027cf9fff7bf9cd2c6d0c0b1db7e0234b766
mchenf <mochen@foss.dk>
2026-05-29 11:12:51 +0800

    Created CRUD endpoints for this controller

--- EwkQxObd.WebApi/Controllers/ewkiqxobd/api/OrganisationController.cs
+++ EwkQxObd.WebApi/Controllers/ewkiqxobd/api/OrganisationController.cs
@@ -1,4 +1,5 @@
-﻿using EwkQxObd.Core.Model.Iqx;
+﻿using EwkQxObd.Core.Model;
+using EwkQxObd.Core.Model.Iqx;
 using EwkQxObd.WebApi.Controllers.ewkiqxobd.Common;
 using EwkQxObd.WebApi.Data;
 using Microsoft.AspNetCore.Mvc;
@@ -24,6 +25,138 @@ namespace EwkQxObd.WebApi.Controllers.ewkiqxobd.api
             _context = dataContext;
             _helper = helper;
         }
+        [HttpPost]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        public async Task<IActionResult> Create([FromBody] Organization NewOrg)
+        {
+            if (NewOrg == null)
+            {
+                return BadRequest(new
+                {
+                    Message = "Must supply a valid, non-null organization."
+                });
+            }
+
+            bool IsDuplicate = await _context.Organization.AnyAsync(c => c.AccountNumber == NewOrg.AccountNumber);
+
+            if (IsDuplicate)
+            {
+                return BadRequest(new
+                {
+                    Message = $"Duplicated ID: {NewOrg.AccountNumber}."
+                });
+            }
+
+            await _context.Organization.AddAsync(NewOrg);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(new
+            {
+                Message = "Organization Created.",
+                New = NewOrg
+            });
+        }
+
+        [HttpGet("{AccountNumber}")]
+        [Produces("application/json")]
+        public async Task<IActionResult> Read([FromRoute] int AccountNumber)
+        {
+            var Found = await _context.Organization.FindAsync(AccountNumber);
+
+            if (Found is null)
+            {
+                return NotFound(new
+                {
+                    Message = $"We did not find organization with account number {AccountNumber}."
+                });
+            }
+
+            return Ok(Found);
+        }
+
+        [HttpPut("{AccountNumber}")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        public async Task<IActionResult> Update([FromRoute] int AccountNumber, [FromBody] Organization UpdateOrg)
+        {
+            if (AccountNumber != UpdateOrg.AccountNumber)
+            {
+                return BadRequest(new
+                {
+                    Message = "Compare carefully the id to target and the one in payload object."
+                });
+            }
+
+            var Found = await _context.Organization.FindAsync(AccountNumber);
+
+
+
+            if (Found is null)
+            {
+                return NotFound(new
+                {
+                    Message = $"Object with ID {AccountNumber} does not exist."
+                });
+            }
+
+            Organization old = new()
+            {
+                AccountNumber = Found.AccountNumber,
+                GeisGuid = Found.GeisGuid,
+                Name = Found.Name,
+                Region = Found.Region,
+                Country = Found.Country,
+                City = Found.City,
+                Street = Found.Street
+            };
+
+            Found.GeisGuid = UpdateOrg.GeisGuid;
+            Found.Name = UpdateOrg.Name;
+            Found.Region = UpdateOrg.Region;
+            Found.Country = UpdateOrg.Country;
+            Found.City = UpdateOrg.City;
+            Found.Street = UpdateOrg.Street;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(new
+            {
+                Message = "Update successful.",
+                Old = old,
+                New = Found
+            });
+        }
+
+        [HttpDelete("{ContractObjectId}")]
+        [Produces("application/json")]
+        public async Task<IActionResult> Delete([FromRoute] int AccountNumber)
+        {
+            var Found = await _context.Organization.FindAsync(AccountNumber);
+
+            if (Found is null)
+            {
+                return NotFound(new
+                {
+                    Message = $"Organization with account number {AccountNumber} does not exist."
+                });
+            }
+
+            _context.Organization.Remove(Found);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(new
+            {
+                Message = $"Organization {AccountNumber} is deleted.",
+                Deleted = Found
+            });
+        }
+
+
+
+
 
         [HttpGet]
         [Produces("application/json")]
@@ -73,19 +206,6 @@ namespace EwkQxObd.WebApi.Controllers.ewkiqxobd.api
             await _context.SaveChangesAsync();
 
             return Ok(new { ContentType = "application/json", Duplicates = dups, Added = added });
-        }
-
-        [HttpPost()]
-        [Consumes("application/json")]
-        public async Task<IActionResult> AddSingle(Organization org)
-        {
-
-
-            await _context.Organization.AddAsync(org);
-
-            await _context.SaveChangesAsync();
-
-            return Ok(new { Consumes = "application/json", Values = org });
         }
 
     }
